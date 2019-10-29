1765e3246f0fad632e3180ce13cb2ead3574f903
1727010 <g1727010@st.t-kougei.ac.jp>
2019-10-29 21:47:17 +0900

    add TitleVoice
    対戦開始ボタンを押すとランダムな「デュエル開始」の音声が流れる

--- 時間処理/Assets/Script/TitleDirector.cs
+++ 時間処理/Assets/Script/TitleDirector.cs
@@ -4,10 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class TitleDirector: MonoBehaviour
 {
-
+    public TitleVoiceSelector TVS;
     //　スタートボタンを押したら実行する
     public void StartGame()
     {
-        SceneManager.LoadScene("ApriScene");
+        TVS.OnClick();
+        Invoke("start_game", TVS.playClip_length());
+    }
+
+    void start_game()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
