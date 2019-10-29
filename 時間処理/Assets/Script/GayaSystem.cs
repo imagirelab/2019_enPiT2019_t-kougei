a74cfe286cfef8d3521048df89e83561467ac1cb
kuro-tt <koki11241124@gmail.com>
2019-10-29 20:06:28 +0900

    一定時間たつとガヤ音声が再生される

--- 時間処理/Assets/Script/GayaSystem.cs
+++ 時間処理/Assets/Script/GayaSystem.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class GayaSystem : MonoBehaviour
 {
+    TimeController TimeLimiter;
+
+    private AudioSource sound01;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimeLimiter = gameObject.AddComponent<TimeController>();
+        TimeLimiter.limit = 6;
+        TimeLimiter.effect = hogehoge;
+        //AudioSourceコンポーネントを取得し、変数に格納
+        sound01 = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void hogehoge()
+    {
+        Debug.Log("hogehoge");
+        sound01.PlayOneShot(sound01.clip);
     }
 }
