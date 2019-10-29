5112df787ebb1217e6775126420f6bb238bd71a5
1727010 <g1727010@st.t-kougei.ac.jp>
2019-10-29 16:23:00 +0900

    時間処理

---
+++ 時間処理/Assets/Script/TimeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    float seconds;
+
+    void Update()
+    {
+
+        seconds += Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enterが押されました");
+            Debug.Log("時間をリセットします");
+        }
+
+        if (seconds >= 10)
+        {
+            seconds = 0;
+            Debug.Log("10秒間何もアクションされませんでした");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
