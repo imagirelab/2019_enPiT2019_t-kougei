0fbf7fb84ff40ef5030f164fe537977783e839f1
1727010 <g1727010@st.t-kougei.ac.jp>
2019-11-05 19:00:09 +0900

    add Life Input & Display
    ライフの数値入力とライフの表示を追加しました

---
+++ 時間処理/Assets/Script/DisplayLife.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayLife : MonoBehaviour
+{
+    public InputManager inputManager;
+    public Text lifeText;
+    private int playerLife;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerLife = inputManager.StartLife;
+        lifeText.text = "LIFE : " + playerLife.ToString();
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            playerLife -= 1;
+        }
+    }
+}
