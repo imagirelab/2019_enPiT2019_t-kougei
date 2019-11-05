4fbd42bff28fceb4a3f9744eca1add236aec93c8
Ushiris_lab <ushiriscco@gmail.com>
2019-11-05 22:21:02 +0900

    Auto stash before merge of "master" and "origin/master"

---
+++ 時間処理/Assets/julius/Script/mic_debug.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mic_debug : MonoBehaviour
+{
+    private TextMesh text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setText(string str)
+    {
+        text.text = str;
+    }
+}
