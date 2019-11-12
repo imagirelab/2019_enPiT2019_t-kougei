139edf0f059d744ed9b69f57eaad0c084d07591a
Ushiris_lab <ushiriscco@gmail.com>
2019-11-12 09:40:33 +0900

    fix mic_debug
    Null refalenceになる不具合を解消しました

--- 時間処理/Assets/julius/Script/mic_debug.cs
+++ 時間処理/Assets/julius/Script/mic_debug.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class mic_debug : MonoBehaviour
 {
-    private TextMesh text;
+    public TextMesh text;
 
     // Start is called before the first frame update
     void Start()
