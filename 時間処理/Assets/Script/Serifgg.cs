ce45375a2d253f04c5ec00ad07ddae935324df16
1727010 <g1727010@st.t-kougei.ac.jp>
2019-11-26 17:05:51 +0900

    やり直し

---
+++ 時間処理/Assets/Script/Serifgg.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Serifgg : MonoBehaviour
+{
+
+    TimeController TimeLimiter;
+
+    public AudioClip Serif01;
+    public AudioClip Serif02;
+    private AudioSource AudioContorole;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeLimiter = gameObject.AddComponent<TimeController>();
+        TimeLimiter.limit = 6;
+        TimeLimiter.effect = Serif;
+        AudioContorole = gameObject.AddComponent<AudioSource>();
+        AudioContorole.clip = Serif01;
+    }
+
+    // Update is called once per frame
+    void Serif()
+    {
+        Debug.Log("セリフが呼ばれました");
+        AudioContorole.PlayOneShot(AudioContorole.clip);
+
+        AudioContorole.clip = Serif02;
+        TimeLimiter.limit = AudioContorole.clip.length;
+        TimeLimiter.ResetTimer();
+        TimeLimiter.effect = serif2;
+    }
+
+    void serif2()
+    {
+        Debug.Log("セリフが呼ばれました");
+        AudioContorole.PlayOneShot(AudioContorole.clip);
+        Destroy(TimeLimiter);
+    }
+}
