93208307deb23719414e90ed7913dbe4f612d66b
Ushiris_lab <ushiriscco@gmail.com>
2019-12-18 20:53:54 +0900

    update voiceManager

--- 時間処理/Assets/Script/TimeController.cs
+++ 時間処理/Assets/Script/TimeController.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class TimeController : MonoBehaviour
 {
-    public float limit = 10;
+    const float MIN_TIME_LIMIT = 0.1f;
+
+    public float limit = 0;
     public delegate void FireMethod();
     public FireMethod effect = delegate () { };
 
     bool isActive = true;
     float seconds;
+
+    void Start()
+    {
+        TimerErrorCheck();
+    }
 
     void Update()
     {
@@ -19,8 +26,9 @@ public class TimeController : MonoBehaviour
 
         if (seconds >= limit)
         {
+            TimerErrorCheck();
+
             seconds = 0;
-            Debug.Log(limit + "秒間何もアクションされませんでした");
             effect();
         }
     }
@@ -33,7 +41,17 @@ public class TimeController : MonoBehaviour
     public void ResetTimer(float newlimit)
     {
         seconds = 0;
+
+        if (TimerErrorCheck()) return;
+
         limit = newlimit;
+    }
+
+    public void ShatDown()
+    {
+        seconds = 0;
+        limit = MIN_TIME_LIMIT;
+        isActive = false;
     }
 
     public void Pause()
@@ -44,5 +62,18 @@ public class TimeController : MonoBehaviour
     public void Resume()
     {
         isActive = true;
+    }
+
+    bool TimerErrorCheck()
+    {
+        if(limit<MIN_TIME_LIMIT)
+        {
+            Debug.Log("too short time limit. does not set?");
+            limit = MIN_TIME_LIMIT;
+
+            return true;
+        }
+
+        return false;
     }
 }
--- 時間処理/Assets/Script/VoiceActor.cs
+++ 時間処理/Assets/Script/VoiceActor.cs
@@ -4,20 +4,70 @@ using UnityEngine;
 
 public class VoiceActor : MonoBehaviour
 {
+    [SerializeField]
+    float breath = 0.1f;
+
+    bool isSpeaking = false;
     Stack<AudioClip> queue;
-    TimeController timer;
     AudioSource mouth;
+    TimeController timer;
 
     void Start()
     {
         mouth = gameObject.GetComponent<AudioSource>();
+
+        timer.ShatDown();
+        timer.effect = Speak;
     }
 
-    void Update()
+    //ここに声を渡すと渡した順に喋ってくれます。
+    public bool SpeakRequest(AudioClip voice)
     {
-        if(queue.Count>0)
-        {
+        queue.Push(voice);
 
+        //まだ発声中か待機中のボイスがあるならキューに追加するだけ
+        if (queue.Count > 1 || isSpeaking)
+        {
+            return false;
         }
+
+        //特になんもなければそのまま喋る
+        Speak();
+    
+        return true;
     }
+
+    //キューされているセリフをすべて忘れます
+    public void Forget()
+    {
+        queue.Clear();
+    }
+
+    //今のセリフの次にvoiceを喋ってくれます。fast=trueなら今のセリフを中断します
+    public void MastSpeak(AudioClip voice, bool fast)
+    {
+
+    }
+
+
+    void Speak()
+    {
+        //喋ることがないならなんもしない
+        if(queue.Count==0)
+        {
+            timer.ShatDown();
+            isSpeaking = false;
+            return;
+        }
+
+        //喋る
+        AudioClip next = queue.Pop();
+        mouth.PlayOneShot(next);
+        isSpeaking = true;
+
+        //次の喋りのタイミングを設定する
+        timer.ResetTimer(next.length);
+    }
+
+
 }
